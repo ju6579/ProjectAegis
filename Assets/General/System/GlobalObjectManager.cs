af5bad2ac66fd38850e96ad8ff341711e6c77f3a
ju6579 <ju6579@naver.com>
2020-11-20 10:04:39 +0900

    Update GlobalObjectManager.cs
    +Particle Object Pool

--- Assets/General/System/GlobalObjectManager.cs
+++ Assets/General/System/GlobalObjectManager.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class GlobalObjectManager : Singleton<GlobalObjectManager>
 {
+    [SerializeField]
+    private List<GameObject> _effectPrefab = new List<GameObject>();
+
+    [SerializeField]
+    private int _effectPoolExtendSize = 5;
+
+    #region Global Prefab Object Pool
     private static int _objectPoolStartSize = 5;
 
     // Prefab, instance
-    private static Dictionary<GameObject, Queue<GameObject>> _globalObjectPool 
+    private static Dictionary<GameObject, Queue<GameObject>> _globalObjectPool
         = new Dictionary<GameObject, Queue<GameObject>>();
 
     // instance, Prefab
-    private static Dictionary<GameObject, GameObject> _instanceCachePool 
+    private static Dictionary<GameObject, GameObject> _instanceCachePool
         = new Dictionary<GameObject, GameObject>();
 
     private static Dictionary<GameObject, int> _globalObjectPoolSizeHash = new Dictionary<GameObject, int>();
 
+    #region MonoBehaviour Callbacks
+    protected override void Awake()
+    {
+        InitializeEffectPool();
+        base.Awake();
+    }
+    #endregion
+
     public static GameObject GetObject(GameObject prefab)
     {
-        if (!_globalObjectPool.ContainsKey(prefab)) 
+        if (!_globalObjectPool.ContainsKey(prefab))
         {
             _globalObjectPool[prefab] = new Queue<GameObject>();
 
@@ -26,7 +41,7 @@ public class GlobalObjectManager : Singleton<GlobalObjectManager>
             _globalObjectPoolSizeHash[prefab] = _objectPoolStartSize;
         }
 
-        if(_globalObjectPool[prefab].Count == 0)
+        if (_globalObjectPool[prefab].Count == 0)
         {
             ExtendObjectPool(prefab, 5);
             _globalObjectPoolSizeHash[prefab] += 5;
@@ -59,6 +74,59 @@ public class GlobalObjectManager : Singleton<GlobalObjectManager>
             _globalObjectPool[prefab].Enqueue(instance);
         }
     }
+    #endregion
 
- 
+    #region Effect Object Pool
+    private Dictionary<CustomEffectType, Queue<GameObject>> _effectObjectPoolHash 
+        = new Dictionary<CustomEffectType, Queue<GameObject>>();
+
+    private Dictionary<CustomEffectType, GameObject> _effectPrefabHash = new Dictionary<CustomEffectType, GameObject>();
+
+    public GameObject GetEffectByPool(CustomEffectType eType)
+    {
+        if (_effectObjectPoolHash[eType].Count <= 0) 
+            ExtendEffectPool(eType, _effectPoolExtendSize);
+
+        return _effectObjectPoolHash[eType].Dequeue();
+    }
+
+    public void ReturnEffectToPool(GameObject instance)
+    {
+        EffectController ec = instance.GetComponent<EffectController>();
+
+        if (ec == null) GlobalLogger.CallLogError(instance.name, GErrorType.InspectorValueException);
+
+        if (instance.activeSelf) instance.SetActive(false);
+
+        _effectObjectPoolHash[ec.EffectType].Enqueue(instance);
+    }
+
+    private void ExtendEffectPool(CustomEffectType eType, int extendAmount)
+    {
+        for (int i = 0; i < extendAmount; i++)
+            _effectObjectPoolHash[eType].Enqueue(Instantiate(_effectPrefabHash[eType]));
+    }
+
+    private void InitializeEffectPool()
+    {
+        _effectPrefab.ForEach((GameObject go) =>
+        {
+            EffectController ec = go.GetComponent<EffectController>();
+            _effectObjectPoolHash[ec.EffectType] = new Queue<GameObject>();
+
+            for (int i = 0; i < _effectPoolExtendSize; i++)
+                _effectObjectPoolHash[ec.EffectType].Enqueue(Instantiate(go));
+
+            _effectPrefabHash.Add(ec.EffectType, go);
+        });
+    }
+    #endregion
+}
+
+public enum CustomEffectType
+{
+    NotSet,
+    Explosion,
+    TrailSmog,
+    Burn
 }
