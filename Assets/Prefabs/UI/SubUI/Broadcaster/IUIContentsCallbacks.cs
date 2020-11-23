b1bd6621e45fba946e3e1ed9b8af58d6d20e2443
ju6579 <ju6579@naver.com>
2020-11-23 16:32:41 +0900

    Delete All File

--- Assets/Prefabs/UI/SubUI/Broadcaster/IUIContentsCallbacks.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine.UI;
-
-using PlayerKindom.PlayerKindomTypes;
-
-public interface IUIContentsCallbacks
-{
-    void OnClickProductContents(Button clicked, ProductionTask pTask);
-    void OnClickShipDataContents(Button clicked, ProductWrapper pWrapper);
-}
